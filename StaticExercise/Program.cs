b38b887d8b8468428a884ae039484e8b57271919
russelibale808 <russelibale808@gmail.com>
2022-11-01 19:16:51 -1000

    StaticExercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var celcius = TempConverter.FahrenheitToCelsius(30); 
+            var fahrenheit = TempConverter.CelciusToFahrenheit(30);
+
+
+            Console.WriteLine("Conversion ");
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+
         }
+
+        
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheitTemp)
+        {
+            var result = (fahrenheitTemp - 32) * 5/9;
+
+            return result;
+        }
+
+        public static double CelciusToFahrenheit(double celsiusTemp)
+        {
+            var result2 =  (celsiusTemp * (9 / 5)) + 32;
+            return result2;
+        }
+    }
+}
